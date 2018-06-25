a87eab3b30adf853226dde2271ae591f1fb42e7e
jmitchell <jacob.mitchell@vml.com>
2018-06-25 12:55:19 -0500

    creates a delete function to demonstrate manipulating the mock data.

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -7,6 +7,18 @@ export function getUsers(){
 	return get('users');
 }
 
+export function deleteUser(id){
+	return del(`users/${id}`);
+}
+
+function del(url){
+	const request = new Request(baseUrl + url, {
+		method: 'DELETE'
+	});
+
+	return fetch(request).then(onSuccess, onError);
+}
+
 function get(url){
 	return fetch(baseUrl + url).then(onSuccess, onError);
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '../index.css';
-import {getUsers} from './api/userApi'
+import {getUsers, deleteUser} from './api/userApi'
 
 // Populate table of users via API call
 getUsers().then(result =>{
@@ -16,4 +16,16 @@ getUsers().then(result =>{
 	});
 
 	global.document.getElementById('users').innerHTML = usersBody;
+
+	const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+	Array.from(deleteLinks, link =>{
+		link.onclick = function(event){
+			const element = event.target;
+			event.preventDefault();
+			deleteUser(element.attributes["data-id"].value);
+			const row = element.parentNode.parentNode;
+			row.parentNode.removeChild(row);
+		};
+	});
 });
